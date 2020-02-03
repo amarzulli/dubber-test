594adaa82e22c826bf49af44e3c872a1d053b063
amarzulli <andy.j@outlook.com>
2020-02-04 10:57:51 +1100

    Add files via upload

--- main.tf
+++ main.tf
@@ -46,15 +46,15 @@ resource ibm_container_vpc_cluster cluster {
 
   zones = [
     {
-      subnet_id = "${ibm_is_subnet.subnet1.id}"
+      subnet_id = "${ibm_is_subnet.subnet[0].id}"
       name      = "${var.ibm_region}-1"
     },
     {
-      subnet_id = "${ibm_is_subnet.subnet2.id}"
+      subnet_id = "${ibm_is_subnet.subnet[1].id}"
       name      = "${var.ibm_region}-2"
     },
     {
-      subnet_id = "${ibm_is_subnet.subnet3.id}"
+      subnet_id = "${ibm_is_subnet.subnet[2].id}"
       name      = "${var.ibm_region}-3"
     }
   ]
