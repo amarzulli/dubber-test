532f54d96e6fa84ea6609e4d733c82ad0c91b455
amarzulli <andy.j@outlook.com>
2020-02-03 15:47:03 +1100

    initial commit

---
+++ provider.tf
@@ -0,0 +1,4 @@
+provider "ibm" {
+  generation = 1
+  region = "au-syd"
+}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable ibm_region {
+    description = "IBM Cloud region where all resources will be deployed"
+    default     = "au-syd"
+}
