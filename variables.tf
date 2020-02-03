850faec141b4a1e796b357219fe4f4fe5266ea0f
amarzulli <andy.j@outlook.com>
2020-02-03 16:11:00 +1100

    added cidr blocks

--- variables.tf
+++ variables.tf
@@ -2,3 +2,13 @@ variable ibm_region {
     description = "IBM Cloud region where all resources will be deployed"
     default     = "au-syd"
 }
+
+##############################################################################
+# Network variables
+##############################################################################
+
+variable "cidr_blocks" {
+    description = "List of CIDR blocks for the subnets"
+    default     = ["10.0.10.0/24", "10.0.11.0/24", "10.0.12.0/24"]
+    type        = "list"  
+}
