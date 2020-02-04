6dcf544a5af424289bac6a837f37a2588bf5cfe7
amarzulli <andy.j@outlook.com>
2020-02-04 15:12:26 +1100

    Create variables.tf

---
+++ VPC/variables.tf
@@ -0,0 +1,32 @@
+##############################################################################
+# Account Variables
+##############################################################################
+
+variable ibm_region {
+    description = "IBM Cloud region where all resources will be deployed"
+    default     = "au-syd"
+}
+
+variable resource_group {
+    description = "Name of resource group to provision resources"
+    default     = "schematics-test"
+}
+
+variable unique_id {
+    description = "The IBM Cloud platform API key needed to deploy IAM enabled resources"
+}
+
+##############################################################################
+
+
+##############################################################################
+# Network variables
+##############################################################################
+
+variable "cidr_blocks" {
+    description = "List of CIDR blocks for the subnets"
+    default     = ["10.0.10.0/24", "10.0.11.0/24", "10.0.12.0/24"]
+    type        = "list"  
+}
+
+##############################################################################
