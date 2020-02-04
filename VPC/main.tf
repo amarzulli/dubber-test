aeacb1bc47abc9cf3385acc54d3097859826b8ff
amarzulli <andy.j@outlook.com>
2020-02-04 15:14:28 +1100

    Update main.tf

--- VPC/main.tf
+++ VPC/main.tf
@@ -1,7 +1,11 @@
 ##############################################################################
 # Resource Group
 ##############################################################################
-
+provider "ibm" {
+  region             = "${var.ibm_region}"
+  generation         = 1
+  ibmcloud_timeout   = 60
+}
 data "ibm_resource_group" "resource_group" {
   name = "${var.resource_group}"
 }
