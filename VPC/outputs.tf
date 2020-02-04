a0caed81f8983efcc0b75d99d38374a1991c54ca
amarzulli <andy.j@outlook.com>
2020-02-04 15:11:31 +1100

    Create outputs.tf

---
+++ VPC/outputs.tf
@@ -0,0 +1,32 @@
+##############################################################################
+# VPC GUID
+##############################################################################
+
+output vpc_id {
+  description = "ID of VPC created"
+  value       = "${ibm_is_vpc.vpc.id}"
+}
+
+##############################################################################
+
+
+##############################################################################
+# List of subnet IDs
+##############################################################################
+
+output subnet_1_id {
+  description = "First Subnet"
+  value       = "${element(ibm_is_subnet.subnet.*.id, 0)}"
+}
+
+output subnet_2_id {
+  description = "Second Subnet"
+  value       = "${element(ibm_is_subnet.subnet.*.id, 1)}"
+}
+
+output subnet_3_id {
+  description = "Third Subnet"
+  value       = "${element(ibm_is_subnet.subnet.*.id, 2)}"
+}
+
+##############################################################################
