d7f77b0988da3883fc48c11219035be8002e0aec
Andrew Marzulli <marzulli@au1.ibm.com>
2020-02-06 12:40:57 +1100

    Update main.tf

--- IKS/main.tf
+++ IKS/main.tf
@@ -9,19 +9,3 @@ data ibm_resource_group resource_group {
 ##############################################################################
 
 
-##############################################################################
-# Schematics outputs from VPC Workspace
-##############################################################################
-
-data ibm_schematics_output vpc {
-  #workspace_id = "${var.vpc_workspace_id}"
-   workspace_id = "{data.ibm_schematics_output.vpc.output_values["workspace_id"]}"
-}
-
-data ibm_schematics_output vpc {
-  #workspace_id = "${var.vpc_workspace_id}"
-  workspace_id = "{data.ibm_schematics_output.vpc.output_values["workspace_id"]}"
-  template_id  = "${element(data.ibm_schematics_workspace.vpc.template_id, 0)}"
-}
-
-##############################################################################
