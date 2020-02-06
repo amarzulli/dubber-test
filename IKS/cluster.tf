e22a28b59618eead5c85532cb784af2c84502135
Andrew Marzulli <marzulli@au1.ibm.com>
2020-02-06 12:51:57 +1100

    IBM Schematics Output

--- IKS/cluster.tf
+++ IKS/cluster.tf
@@ -5,22 +5,22 @@
 resource ibm_container_vpc_cluster cluster {
 
   name               = "${var.cluster_name}"
-  vpc_id             = "${data.ibm_schematics_output.VPC_IKS.output_values["vpc_id"]}"
+  vpc_id             = "${data.ibm_schematics_output.vpc.output_values["vpc_id"]}"
   flavor             = "${var.machine_type}"
   worker_count       = "${var.worker_count}"
   resource_group_id  = "${data.ibm_resource_group.resource_group.id}"
 
   zones = [
     {
-      subnet_id = "${data.ibm_schematics_output.VPC-IKS.output_values["subnet_1_id"]}"
+      subnet_id = "${data.ibm_schematics_output.vpc.output_values["subnet_1_id"]}"
       name      = "${var.ibm_region}-1"
     },
     {
-      subnet_id = "${data.ibm_schematics_output.VPC-IKS.output_values["subnet_2_id"]}"
+      subnet_id = "${data.ibm_schematics_output.vpc.output_values["subnet_2_id"]}"
       name      = "${var.ibm_region}-2"
     },
     {
-      subnet_id = "${data.ibm_schematics_output.VPC-IKS.output_values["subnet_3_id"]}"
+      subnet_id = "${data.ibm_schematics_output.vpc.output_values["subnet_3_id"]}"
       name      = "${var.ibm_region}-3"
     }
   ]
